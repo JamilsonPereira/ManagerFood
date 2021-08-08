aa1da584eb5d113bc9c6e8cbdb6f858bbcb264ab
JamilsonPereira <jamilson.nunesp@hotmail.com>
2021-08-08 17:39:19 -0300

    Primeiro commit no Mfood

---
+++ src/main/java/br/com/managerfood/cmdb/MsvMangerfoodCmdbApplication.java
@@ -0,0 +1,14 @@
+package br.com.managerfood.cmdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvMangerfoodCmdbApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvMangerfoodCmdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/managerfood/cmdb/MsvMangerfoodCmdbApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.managerfood.cmdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvMangerfoodCmdbApplicationTests {
+
+
+	@Test
+	void contextLoads() {
+	}
+
+}
